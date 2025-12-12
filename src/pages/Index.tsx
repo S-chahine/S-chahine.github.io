4e416840ed559f044c999453f593bb2bd4c72d49
S-chahine <samah_chahine@outlook.com>
2025-12-11 23:47:32 -0500

    update project section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,9 +1,8 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
-import SkillsSection from "@/components/skillsSection";
+import SkillsSection from "@/components/SkillsSection";
 import ProjectsSection from "@/components/ProjectsSection";
-import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
 
 const Index = () => {
